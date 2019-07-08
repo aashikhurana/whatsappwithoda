17a4a6bd993c30727f73ea98db356e5c000a684b
aashikhurana <aashi.khurana@gmail.com>
2019-07-08 10:00:31 +0800

    Add files via upload

--- webhook/config/Config.js
+++ webhook/config/Config.js
@@ -1,12 +1,12 @@
 // ODA Details
-module.exports.ODA_WEBHOOK_URL = process.env.BOT_WEBHOOK_URL || 'ODA_WEBHOOK_URL';
+module.exports.ODA_WEBHOOK_URL = process.env.BOT_WEBHOOK_URL || 'https://whatsappdigitalassistant.herokuapp.com/bot/message';
 module.exports.ODA_WEBHOOK_SECRET = process.env.BOT_WEBHOOK_SECRET || 'ODA_WEBHOOK_SECRET';
 
 // Smooch Details
-exports.SMOOCH_APP_ID = process.env.SMOOCH_APP_ID || 'SMOOCH_APP_ID';
-exports.SMOOCH_KEY_ID = process.env.SMOOCH_KEY_ID || 'SMOOCH_KEY_ID';
-exports.SMOOCH_SECRET = process.env.SMOOCH_SECRET || 'SMOOCH_SECRET';
-exports.SMOOCH_WEBHOOK_SECRET = process.env.SMOOCH_WEBHOOK_SECRET || 'SMOOCH_WEBHOOK_SECRET';
+exports.SMOOCH_APP_ID = process.env.SMOOCH_APP_ID || '5cda2f657d26180010c43fce';
+exports.SMOOCH_KEY_ID = process.env.SMOOCH_KEY_ID || 'app_5d22a07b58da210011c1cae8';
+exports.SMOOCH_SECRET = process.env.SMOOCH_SECRET || '8111cFgMHZVlvbduW75LVoiz05LucHGzOnjzzw5NF3iulZCXO9XdYUIaO0p3IcNDPWe_krYBSE-T8Pw-O9gzmw';
+exports.SMOOCH_WEBHOOK_SECRET = process.env.SMOOCH_WEBHOOK_SECRET || 'lkYbkgrB6c2xbkYVleCmERAdeDVf9lhPyck3E1a9KRkBdF_GVMaTS1Cw_-X5KOZPAF-Ik0auPWO-TP28yAiOPg';
 
 // General Details
 exports.PROXY = process.env.PROXY || process.env.http_proxy;
