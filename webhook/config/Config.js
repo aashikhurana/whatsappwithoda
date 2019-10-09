0cbb7c82ab41122b291ad679f58ee8e9999a2cf6
aashikhurana <aashi.khurana@gmail.com>
2019-10-09 11:26:26 +0800

    Add files via upload

--- webhook/config/Config.js
+++ webhook/config/Config.js
@@ -1,12 +1,12 @@
 // ODA Details
-module.exports.ODA_WEBHOOK_URL = process.env.BOT_WEBHOOK_URL || 'https://botphx1I0014H76DC67bots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-100b89d671b54afca3069fe360e4bad4/listeners/webhook/channels/2c5316e8-9acc-4585-a707-b01f5015a319';
-module.exports.ODA_WEBHOOK_SECRET = process.env.BOT_WEBHOOK_SECRET || 'GtXasIsAhk4QKE85QNnf1SKFubiOOvyB';
+module.exports.ODA_WEBHOOK_URL = process.env.BOT_WEBHOOK_URL || 'https://botv2iad1I0225H0A2C1Dbots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-100b89d671b54afca3069fe360e4bad4/listeners/webhook/channels/c5510f9b-4ec4-4baa-b4aa-e87a883dc693';
+module.exports.ODA_WEBHOOK_SECRET = process.env.BOT_WEBHOOK_SECRET || 'mrDUrrNh8tVj7eA6XTVxSnD5O2WYZj6M';
 
 // Smooch Details
-exports.SMOOCH_APP_ID = process.env.SMOOCH_APP_ID || '5cda2f657d26180010c43fce';
-exports.SMOOCH_KEY_ID = process.env.SMOOCH_KEY_ID || 'app_5d22a07b58da210011c1cae8';
-exports.SMOOCH_SECRET = process.env.SMOOCH_SECRET || '8111cFgMHZVlvbduW75LVoiz05LucHGzOnjzzw5NF3iulZCXO9XdYUIaO0p3IcNDPWe_krYBSE-T8Pw-O9gzmw';
-exports.SMOOCH_WEBHOOK_SECRET = process.env.SMOOCH_WEBHOOK_SECRET || 'lkYbkgrB6c2xbkYVleCmERAdeDVf9lhPyck3E1a9KRkBdF_GVMaTS1Cw_-X5KOZPAF-Ik0auPWO-TP28yAiOPg';
+exports.SMOOCH_APP_ID = process.env.SMOOCH_APP_ID || '5d7b41ecaa6a4d0011ebd702';
+exports.SMOOCH_KEY_ID = process.env.SMOOCH_KEY_ID || 'SMOOCH_KEY_ID';
+exports.SMOOCH_SECRET = process.env.SMOOCH_SECRET || '3BZzNFG0mTdGpyurvefl7Tj6Q6W6tzn19DiLxOY3eho3bf53npH3zs207mnEo1Ixh9-eCqvH3cq2eCbI3tsPuQ';
+exports.SMOOCH_WEBHOOK_SECRET = process.env.SMOOCH_WEBHOOK_SECRET || 'TzcdTiXIm5l_SBpUumkTxZ04MyHwZCDEGpjnIMKcjMD19sSi9tXC__snLlWQG28nQMHFENlfe81xIcYHLXtdZg';
 
 // General Details
 exports.PROXY = process.env.PROXY || process.env.http_proxy;
